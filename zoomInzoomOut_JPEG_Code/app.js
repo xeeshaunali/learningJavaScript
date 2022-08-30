03734a35ff0e390c2a1df52e581220e169f7d8cc
xeeshaunali <shaniijan786@gmail.com>
2022-08-30 10:40:20 +0500

    Learning JS

---
+++ zoomInzoomOut_JPEG_Code/app.js
@@ -0,0 +1,9 @@
+function zoomin(){
+    var a = document.getElementById('jpeg')
+    a.className = 'zoomin'
+}
+
+function zoomout(){
+    var a = document.getElementById('jpeg')
+    a.className = 'zoomout'
+}
