8a6680248a16bb3aabf362b5cfd8cd9858576b01
xeeshaunali <shaniijan786@gmail.com>
2022-08-31 12:32:10 +0500

    Learning JS

--- tagName_target_JS/app.js
+++ tagName_target_JS/app.js
@@ -1,7 +1,17 @@
-function change(){
-    var a = document.getElementById('link')
-    a.href = 'https://www.youtube.com'
+// function change(){
+//     var a = document.getElementById('link')
+//     a.href = 'https://www.youtube.com'
+// }
+
+
+
+function styleAllTag(){
+    var a = document.getElementsByTagName('p')    
+    for (var i = 0; i < a.length; i++){
+        a[i].style.backgroundColor = 'red'
+        a[i].style.color='white'
+    }
+    
+    
 }
-
-
 
