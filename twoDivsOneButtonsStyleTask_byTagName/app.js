88520e85707ffc31fbde1657b2972010b079e248
xeeshaunali <shaniijan786@gmail.com>
2022-08-31 13:59:14 +0500

    Learning JS

---
+++ twoDivsOneButtonsStyleTask_byTagName/app.js
@@ -0,0 +1,14 @@
+function styleOneDiv(){
+var a = document.getElementById('para')
+a.className = 'oneDiv'
+
+
+var b = document.getElementsByTagName('p')
+for (var i = 0; i < b.length; i++){
+    if(i<4){
+        b[i].className = 'one'
+        b[i].innerHTML = 'Xeeshaun Ali Narejo Data Coder'
+    }
+}
+
+}
