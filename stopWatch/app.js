5e8ff15e6b4843fa13289208ff502866bfa72948
xeeshaunali <shaniijan786@gmail.com>
2022-09-01 14:09:15 +0500

    Learning JS

---
+++ stopWatch/app.js
@@ -0,0 +1,25 @@
+
+    // setInterval method
+    /*
+    setInterval(function(){
+        document.write("Xee" + "<br>")
+    }, 1000)
+    */
+
+
+    // OTHER WAY OF USING setInterval Method
+
+    /*
+    function foo(){
+        document.write('xee' + "<br>")
+    }
+
+    setInterval(foo,1000)
+    */
+
+
+    var a = 0
+    setInterval(function(){
+        a++
+        document.write(a)
+    },1000)
