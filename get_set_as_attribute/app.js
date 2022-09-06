3a2a1df8f7ce9abd6f2e6f23a2fa5c93e6f4cc1d
xeeshaunali <shaniijan786@gmail.com>
2022-09-06 12:55:45 +0500

    Learning Js

--- get_set_as_attribute/app.js
+++ get_set_as_attribute/app.js
@@ -6,28 +6,43 @@ msec = 0
 var a = document.getElementById('msec')
 var b = document.getElementById('sec')
 var c = document.getElementById('min')
+var interval;
 
 
 
 function start(){
-   setInterval(function(){
-    msec++
+
+ interval =   setInterval(function(){
+            msec++
+            a.innerHTML = msec
+                if(msec >= 100){
+                   sec++
+                   b.innerHTML = sec
+                   msec = 0 
+                   if(sec >=60){
+                                        min++
+                                        c.innerHTML = min
+                                        sec = 0
+                                    }         
+                }
+        }, 10);
+        var i = document.getElementById('start')
+        i.disabled = true
+}
+
+
+function stop(){
+    clearInterval(interval)
+}
+
+
+function reset(){
+    msec = 0
+    sec = 0
+    min = 0
+
     a.innerHTML = msec
-
-        if(msec >= 100){
-           sec++
-           b.innerHTML = sec 
-           msec = 0      
-
-           if(sec >= 60){
-           min++
-           c.innerHTML = min
-           
-        }
-        }            
-
-
-    
-   }, 10);
+    b.innerHTML = sec
+    c.innerHTML = min
 }
 
