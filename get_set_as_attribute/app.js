9bc0d7316eeade5f3fd1a0e2455ab8c83e9c9f03
xeeshaunali <shaniijan786@gmail.com>
2022-09-06 12:41:47 +0500

    Learning JS

--- get_set_as_attribute/app.js
+++ get_set_as_attribute/app.js
@@ -5,19 +5,26 @@ msec = 0
 
 var a = document.getElementById('msec')
 var b = document.getElementById('sec')
-var b = document.getElementById('min')
-
+var c = document.getElementById('min')
 
 
 
 function start(){
    setInterval(function(){
-    
     msec++
     a.innerHTML = msec
+
         if(msec >= 100){
-                    
+           sec++
+           b.innerHTML = sec 
+           msec = 0      
+
+           if(sec >= 60){
+           min++
+           c.innerHTML = min
+           
         }
+        }            
 
 
     
