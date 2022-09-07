ab1e6b241fccbd96cba2974cd15afe1c2b81e1e0
xeeshaunali <shaniijan786@gmail.com>
2022-09-07 13:07:52 +0500

    learning JS

--- get_set_as_attribute/app.js
+++ get_set_as_attribute/app.js
@@ -1,2 +1,13 @@
 
-console.log(document.childNodes[1].childNodes[0])
+// console.log(document.childNodes[1].childNodes[0].childNodes[0])
+
+
+// hasattribute()
+// setattribute()
+// getattribute() 
+
+function foo(){
+var a = document.getElementById('abc')
+var b = a.hasAttribute('class')
+console.log(b)
+}
