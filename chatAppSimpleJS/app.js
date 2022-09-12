e7511bf2ffe9ac849b8e1b60da9df4222e6c9c9a
xeeshaunali <shaniijan786@gmail.com>
2022-09-12 14:14:41 +0500

    learning JS

---
+++ chatAppSimpleJS/app.js
@@ -0,0 +1,4 @@
+var nayaParagraphJS = document.createElement('p')
+var nayaTextJs = document.createTextNode('Naya text Paragraph me JS sy')
+nayaParagraphJS.append(nayaTextJs)
+console.log(nayaParagraphJS)
