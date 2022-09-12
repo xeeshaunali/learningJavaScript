059dcaccda151f7abc2fd92c55a827500d868d4c
xeeshaunali <shaniijan786@gmail.com>
2022-09-12 12:58:57 +0500

    learning JS

--- get_set_as_attribute/app.js
+++ get_set_as_attribute/app.js
@@ -1,35 +1,15 @@
-
-// console.log(document.childNodes[1].childNodes[0].childNodes[0])
-
-
-// hasattribute()
-// setattribute()
-// getattribute() 
-
-//hasAttribute
-
-// function foo(){
-//     var a = document.getElementById('has')
-//     var b = a.hasAttribute('class')
-//     var c = a.getAttribute('class')
-//     var d = a.setAttribute('name', 'zee')
-//     console.log(b)
-//     console.log(c)
-//     console.log(d)
-
-
-    
-// }
-
-
-
 //getAttribute
-
 /*
 function foo(){
-    var a = document.getElementById('set')
+    var a = document.getElementById('abc')
     var b = a.getAttribute('class')
     console.log(b)
+
 }
 */
 
+function foo(){
+var a = document.getElementById('abc')
+var b = a.setAttribute('class', 'change')
+console.log(b)
+}
