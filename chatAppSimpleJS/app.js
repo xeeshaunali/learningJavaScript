2e31d38c73dec4a336f4ebb05a21e0685b962dde
xeeshaunali <shaniijan786@gmail.com>
2022-09-13 11:38:30 +0500

    Learning JS

--- chatAppSimpleJS/app.js
+++ chatAppSimpleJS/app.js
@@ -1,17 +1,4 @@
-// function foo(){
-//     var a = document.getElementById('abc')
-//     var newElement = document.createElement('p')
-//     newElement.setAttribute('id','shush')
-//     var b = document.getElementById('shush')
-//     console.log(newElement)
-//     var newMsg = document.createTextNode(a.value)
-//     newElement.appendChild(newMsg)
-//     res = newElement.appendChild(newMsg)
-//     document.getElementById("body").appendChild(res);
-    
-    
-    
-// }
+
 
 function foo(){
 var a = document.getElementById('chat')
