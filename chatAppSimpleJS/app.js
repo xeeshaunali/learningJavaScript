96c597d97e14f7cc1c2e00892b7847363b72a24e
xeeshaunali <shaniijan786@gmail.com>
2022-09-13 11:22:36 +0500

    Learning JS

--- chatAppSimpleJS/app.js
+++ chatAppSimpleJS/app.js
@@ -1,14 +1,24 @@
+// function foo(){
+//     var a = document.getElementById('abc')
+//     var newElement = document.createElement('p')
+//     newElement.setAttribute('id','shush')
+//     var b = document.getElementById('shush')
+//     console.log(newElement)
+//     var newMsg = document.createTextNode(a.value)
+//     newElement.appendChild(newMsg)
+//     res = newElement.appendChild(newMsg)
+//     document.getElementById("body").appendChild(res);
+    
+    
+    
+// }
+
 function foo(){
-    var a = document.getElementById('abc')
-    var newElement = document.createElement('p')
-    newElement.setAttribute('id','shush')
-    var b = document.getElementById('shush')
-    console.log(newElement)
-    var newMsg = document.createTextNode(a.value)
-    newElement.appendChild(newMsg)
-    res = newElement.appendChild(newMsg)
-    document.getElementById("body").appendChild(res);
-    
-    
-    
+var a = document.getElementById('chat')
+var newElement = document.createElement('p')
+var newMsg = document.createTextNode(a.value)
+newElement.setAttribute('id','shush')
+var para = document.getElementById('shush')
+newElement.append(newMsg)
+var paragraph = document.getElementById('myDiv').appendChild(newElement)
 }
