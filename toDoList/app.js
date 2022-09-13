458e2be5ab3ea598525a877ee6db93e049980785
xeeshaunali <shaniijan786@gmail.com>
2022-09-13 14:54:33 +0500

    Learning JS

--- toDoList/app.js
+++ toDoList/app.js
@@ -4,7 +4,19 @@ function foo(){
     var newLi = document.createElement('li')
     var newMsg = document.createTextNode(docs.value)
     newLi.appendChild(newMsg) 
-    var a = document.getElementById('myDiv').appendChild(newLi)   
+    var a = document.getElementById('myDiv').appendChild(newLi)
+    docs.value = ''   
 
 }
 
+
+/*
+var a = document.getElementById ( ' abc ' )
+function bhejo ( ) {
+    var add = document.getElementById ( ' addtodo ' )
+    var li = document.createElement ( ' li ' )
+   var textinsert = document.createTextNode ( add.value )
+    li.appendChild ( textinsert )
+    a.appendChild ( li )
+}
+*/
