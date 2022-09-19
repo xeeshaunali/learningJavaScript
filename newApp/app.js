17c9a26e3724f5a93ca8e24f5d70100513178f9c
xeeshaunali <shaniijan786@gmail.com>
2022-09-19 10:51:22 +0500

    Learning JS

---
+++ newApp/app.js
@@ -0,0 +1,22 @@
+//getAttribute
+/*
+function foo(){
+    var a = document.getElementById('abc')
+    var b = a.getAttribute('class')
+    console.log(b)
+
+}
+*/
+
+
+//SETATTRIBUTE
+
+/*
+function foo(){
+var a = document.getElementById('abc')
+var b = a.setAttribute('class', 'change')
+console.log(b)
+}
+*/
+
+
