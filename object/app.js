d4c75fcbce7aebcd3b509f74ca8eb83e6300c9c2
xeeshaunali <shaniijan786@gmail.com>
2022-09-20 11:00:02 +0500

    Learning JS

--- object/app.js
+++ object/app.js
@@ -1,9 +1,15 @@
 //OBJECT
 
-var obj = {
-    name1 : 'Saad',
-    age : 22,
-    arr :['Ali','Syed','03213008992']
-} 
+// var obj = {
+//     name1 : 'Saad',
+//     age : 22,
+//     arr :['Ali','Syed','03213008992'],
+// } 
 
-console.log(obj)
+// // console.log(obj)
+// // document.write(obj)
+// document.write(obj.arr[0])
+
+
+var arr = ['saad', 'ali', 0000001]
+console.log(arr)
