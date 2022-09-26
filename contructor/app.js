428bc9b72564e7f6fc48f94cb44592323c9a9e73
xeeshaunali <shaniijan786@gmail.com>
2022-09-26 14:43:29 +0500

    Learning JS

--- contructor/app.js
+++ contructor/app.js
@@ -17,6 +17,15 @@
 // localStorage.clear()
 // sessionStorage.clear()
 
-
-function signup(){var fname = document.getElementById('fname').value;localStorage.setItem('First Name', fname);var lname = document.getElementById('lname').value;localStorage.setItem('last Name', lname);var email1 = document.getElementById('email1').value;localStorage.setItem('Email', email1);var pass1 = document.getElementById('pass1').value;localStorage.setItem('Password', pass1);var pass2 = document.getElementById('pass2').value;localStorage.setItem('PasswordConfirm', pass2);
+//SIGNUP FORM
+function signup(){var fname = document.getElementById('fname').value;
+                    localStorage.setItem('First Name', fname);
+                    var lname = document.getElementById('lname').value;
+                    localStorage.setItem('last Name', lname);
+                    var email1 = document.getElementById('email1').value;
+                    localStorage.setItem('Email', email1);
+                    var pass1 = document.getElementById('pass1').value;
+                    localStorage.setItem('Password', pass1);
+                    var pass2 = document.getElementById('pass2').value;
+                    localStorage.setItem('PasswordConfirm', pass2);
 }
