f8df04ac1a67e375fc4340afbec9d2a927c70767
xeeshaunali <shaniijan786@gmail.com>
2022-09-26 10:40:30 +0500

    Learning JS

--- contructor/app.js
+++ contructor/app.js
@@ -3,9 +3,19 @@
 //localStorage.clear()
 //localStorage.clear()
 
-
-localStorage()
 //localStorage() accepts objects 
 // key and value as in object
+
+// localStorage.setItem('age', 22)
+// localStorage.setItem('name','Saad')
+// sessionStorage.setItem('name', 'Fariha')
+// sessionStorage.setItem('age', 100)
+
+// document.write(localStorage.getItem('name'), localStorage.getItem('age'), localStorage.getItem('non-Existance')
+// ) 
+
+localStorage.clear()
+sessionStorage.clear()
+
 
 
