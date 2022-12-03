175b4c56d49b4b34148c5730225fc5b156caff4f
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2022-12-03 12:43:55 +0500

    Work in progress

---
+++ revision/app.js
@@ -0,0 +1,11 @@
+
+function openNav(){
+    alert("Working")
+    document.getElementById('mySidenavv').style.width = "250px";
+    
+}
+
+function closeNav(){
+    alert('Working')
+    document.getElementById('closeNavv').style.width = "0"
+}
